1e13befc7145c4738bc2568eec1c26d8722c6ba1
Tseng\Bevis <bevis@binodata.com>
2018-08-31 19:53:54 +0800

    add database project and unit test project for student

---
+++ Test/OutSource.UnitTest/TestUserInsert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OutSource.EF;
+
+namespace OutSource.UnitTest
+{
+    [TestClass]
+    public class TestUserInsert
+    {
+        [TestMethod]
+        public void Test_User_Data_Insert()
+        {
+            Users user = new Users();
+            user.Address = "台北市中山區";
+            user.Name = "黃小名";
+            user.Email = "cc@bino.com";
+            user.CellPhone = "0912123123";
+            user.CreateTime = DateTime.Now;
+            user.UpdateTime = DateTime.Now;
+
+            using (var db = new OUT_SOURCE_DBEntities())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
